5ed5ca5d71cebd8e57d7e7d7748bc08b7f35f5ba
TolkachevPeter <peter.tolkachev@gmail.com>
2020-10-22 14:04:17 +0300

    fix Joi uri

--- joi-models/link-model.js
+++ joi-models/link-model.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const { Joi } = require('celebrate');
 
-module.exports.linkJoi = Joi.string().uri();
+module.exports.linkJoi = Joi.string().uri().regex(/^(?!.*:0000)(?!.*\/\/\/)https?:\/\/(www.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(\w{1,}(-\w+)*\.[a-z]{2,}))((.?[a-zA-Z0-9\/-])*(:[1-9]\d{1,4})?)#?$/);
