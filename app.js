e8f216c8118f7b0498b734cfc291985fa5786539
Tolkachev Peter <peter.tolkachev@gmail.com>
2020-10-23 17:35:56 +0300

    fix bugs

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(errorLogger);
 
 app.use(errors());
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
@@ -56,7 +57,7 @@ app.use((err, req, res, next) => {
       // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
-        : message
+        : message,
     });
 });
 
