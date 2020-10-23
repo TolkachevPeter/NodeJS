2058994b1ba956ef045465ae034e4d137143cae9
Tolkachev Peter <peter.tolkachev@gmail.com>
2020-10-23 17:37:45 +0300

    fix bugs

--- joi-models/password-model.js
+++ joi-models/password-model.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
 const { Joi } = require('celebrate');
 
+// eslint-disable-next-line no-useless-escape
 module.exports.passwordJoi = Joi.string().required().pattern(new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'));
